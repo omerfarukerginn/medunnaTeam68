587375a9a12dc5163f0a4833721fb4e285a529df
Zulal Akbulut <zulalakbulut2516@gmail.com>
2022-08-04 14:15:46 +0200

    04.08.2022 zulal2

---
+++ src/test/java/stepDefinitions/apiStepDefinitions/US_001_API_StepDefinition.java
@@ -0,0 +1,8 @@
+package stepDefinitions.apiStepDefinitions;
+
+public class US_001_API_StepDefinition {
+    public static void main(String[] args) {
+        System.out.println("Arkadaslar main kismi github'a gonderirken problem yasamamak icin eklenmistir, silebilirsiniz");
+
+    }
+}
---
+++ src/test/java/stepDefinitions/dataBaseStepDefinitions/US_001_DataBase_StepDefinition.java
@@ -0,0 +1,7 @@
+package stepDefinitions.dataBaseStepDefinitions;
+
+public class US_001_DataBase_StepDefinition {
+    public static void main(String[] args) {
+        System.out.println("Arkadaslar main kismi github'a gonderirken problem yasamamak icin eklenmistir, silebilirsiniz");
+    }
+}

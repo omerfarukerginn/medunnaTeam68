7cbd95f68830f7e523d13a2463929d1dc931b759
Mehmet Nail Kocagoz <mehmetnailkocagoz@gmail.com>
2022-08-06 00:06:55 +0200

    ...

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,4 +4,7 @@ public class LoginPage {
     public static void main(String[] args) {
         System.out.println("Arkadaslar main kismi github'a gonderirken problem yasamamak icin eklenmistir, silebilirsiniz");
     }
+
+
+
 }

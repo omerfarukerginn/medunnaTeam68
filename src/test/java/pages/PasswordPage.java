22d3484080fcdaabc0288fb41548e402ccc58a0e
Ömer Faruk Ergin <omerfarukerginn@gmail.com>
2022-08-10 21:08:07 +0300

    PasswordPage

--- src/test/java/pages/PasswordPage.java
+++ src/test/java/pages/PasswordPage.java
@@ -1,7 +1,31 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class PasswordPage {
-    public static void main(String[] args) {
-        System.out.println("Arkadaslar main kismi github'a gonderirken problem yasamamak icin eklenmistir, silebilirsiniz");
+    public PasswordPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//a[@href='/account/password']")
+    public WebElement homePagePasswordButton;
+
+    @FindBy(xpath = "//input[@name='currentPassword']")
+    public WebElement currentPasswordTextArea;
+
+    @FindBy(name = "newPassword")
+    public WebElement newPasswordTextArea;
+
+    @FindBy(xpath = "//li[@class='point'][2]")
+    public WebElement passwordStrengtLevel2;
+
+    @FindBy(name = "ConfirmPassword")
+    public WebElement confirmPassordTextArea;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement passwordPageSaveButton;
+
 }

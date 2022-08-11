41178bd735fa708be5188bc3e8861c54ecf86aee
Ebru Aydin <aydinnebrr@gmail.com>
2022-08-11 17:18:54 +0200

    08.11.2022

--- src/test/java/baseUrl/MedunnaBaseUrl.java
+++ src/test/java/baseUrl/MedunnaBaseUrl.java
@@ -2,22 +2,13 @@ package baseUrl;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import utilities.ConfigReader;
 
 public class MedunnaBaseUrl {
 
     public static RequestSpecification spec;
-<<<<<<< HEAD
-
-    public static void medunnaSetup(){
-        //spec = new RequestSpecBuilder().setBaseUri("https://medunna.com").build();
-        spec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("base_url")).build();
-
-=======
     public static void medunnaSetup(){
 
         spec = new RequestSpecBuilder().setBaseUri("https://medunna.com").build();
->>>>>>> main
 
     }
 

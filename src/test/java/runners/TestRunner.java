cf10c140f04948a6f47dbb1fa03d32796f0f36a4
Ömer Faruk Ergin <omerfarukerginn@gmail.com>
2022-08-12 00:34:04 +0300

    ofe

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -12,26 +12,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-<<<<<<< HEAD
-        tags = "@US_005_TC_009",
-=======
-<<<<<<< HEAD
-
-        tags = "@US_011",
-
-=======
-<<<<<<< HEAD
-        tags = "@api10",
-=======
-<<<<<<< HEAD
-        tags = "@US_011",
-=======
-        tags = "@US_014",
->>>>>>> main
->>>>>>> main
->>>>>>> main
->>>>>>> main
-        dryRun = false
+        tags = "@US_008",
+        dryRun = true
 )
 
 public class TestRunner {

b4549000168f72b02f441afa9192d2993cdd5b09
makifoeren <makif.oeren@gmail.com>
2022-08-23 10:36:58 +0200

    US_023

---
+++ src/test/java/stepDefinitions/uiStepDefinitions/US_023_UI_StepDefinition.java
@@ -0,0 +1,74 @@
+package stepDefinitions.uiStepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US_023_UI_StepDefinition {
+
+
+    @Given("Kullanici staff olarak giris yapar")
+    public void kullanici_staff_olarak_giris_yapar() {
+
+    }
+
+    @Given("staff my pages menusunu tiklar")
+    public void staff_my_pages_menusunu_tiklar() {
+
+    }
+
+    @Given("staff search patient buttonunu tiklar")
+    public void staff_search_patient_buttonunu_tiklar() {
+
+    }
+
+    @And("staff arama kutusuna SSN ile hastayi arar")
+    public void staffAramaKutusunaSSNIleHastayiArar() {
+        
+    }
+
+    @Given("staff show appointment buttonunu tiklar")
+    public void staff_show_appointment_buttonunu_tiklar() {
+
+    }
+
+    @Given("staff hastanin durumunun completede veya cancelled oldugunu dogrular")
+    public void staff_hastanin_durumunun_completede_veya_cancelled_oldugunu_dogrular() {
+
+    }
+
+    @Given("staff payment invoice buttonunu tiklar")
+    public void staff_payment_invoice_buttonunu_tiklar() {
+
+    }
+
+    @Then("staf muayene ucreti, test urun fiyatlari, bilgilerini goruntuleyebilir")
+    public void staf_muayene_ucreti_test_urun_fiyatlari_bilgilerini_goruntuleyebilir() {
+
+    }
+
+    @Then("staff hasta icin create inovoice tiklayarak fatura olusturur")
+    public void staff_hasta_icin_create_inovoice_tiklayarak_fatura_olusturur() {
+        ;
+    }
+
+    @Then("staff olusturdugu faturayi kaydeder")
+    public void staff_olusturdugu_faturayi_kaydeder() {
+
+    }
+
+    @Then("staff basari ile kaydedildi msj i dogrular")
+    public void staff_basari_ile_kaydedildi_msj_i_dogrular() {
+
+    }
+
+
+
+    @Then("staf fatura sayfasini dogrular")
+    public void staf_fatura_sayfasini_dogrular() {
+
+
+    }
+
+
+}
---
+++ src/test/resources/features/uiFeatures/US_023_UI.feature
@@ -0,0 +1,47 @@
+@US_023
+Feature: Fatura (Staff) olusturma ve assert etme
+
+  Background: Ortak adimlar
+    Given Kullanici ana sayfaya gider
+    And Kullanici staff olarak giris yapar
+    And staff my pages menusunu tiklar
+    And staff search patient buttonunu tiklar
+    And staff arama kutusuna SSN ile hastayi arar
+    And staff show appointment buttonunu tiklar
+
+
+  Scenario: Kullanıcı (Staff) fatura olusturabilmeli ve olusturdugu faturanin icerigini test edebilmeli
+    Given staff hastanin durumunun completede veya cancelled oldugunu dogrular
+    And staff payment invoice buttonunu tiklar
+    Then staf muayene ucreti, test urun fiyatlari, bilgilerini goruntuleyebilir
+    And staff hasta icin create inovoice tiklayarak fatura olusturur
+    And staff olusturdugu faturayi kaydeder
+    Then staff basari ile kaydedildi msj i dogrular
+    And Sayfayi kapatir
+
+  Scenario: Kullanici olusturdgu faturayi kontrol edebilmeli
+    Given staff payment invoice buttonunu tiklar
+    Then staf fatura sayfasini dogrular
+    And Sayfayi kapatir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

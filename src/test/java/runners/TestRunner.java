147c946d5ca061cd07b7b74abf53131eb1821770
makifoeren <makif.oeren@gmail.com>
2022-08-24 10:41:17 +0200

    yeni raporlama eklendi

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.junit.Cucumber;
 
         features = "src/test/resources/features/uiFeatures",
         glue = "stepDefinitions/uiStepDefinitions",
-        tags = "@US_023",
+        tags = "@US_024",
         dryRun = false
 )
 
